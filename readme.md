6a5a10f498bd997389eee4e2c63c8acd5e3fc717
Hnin-Nu-Hlaing <sakurayk2002@gmail.com>
2024-09-01 20:45:22 +0630

    add readme file and add js codes

---
+++ readme.md
@@ -0,0 +1,9 @@
+substing() method
+- parameterနှစ်ခုရှိ
+- start end
+- index numberရဲ့ နောက်ကဟာပဲယူတာ
+
+pig latin (start end)
+- ပြောင်းပြန်ယူတာ
+
+function ထဲမှာ နောက်ထပ်functionထပ်ခေါ်တာ,functionထဲမှာ object ၂ခုဖြစ်သွားလို့ method
--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-console.log("Hello")
+function Person (name, hobbies) {
+    this.name = name;
+    this.hobbies = hobbies;
+
+    this.listPerson = function () {
+        return this.hobbies.map ((function(hobby) {
+            // console.log(this);
+            console.log(this.name + "   loves " + hobby)
+        }),this)
+    }
+}
+
+let person = new Person ('Hnin', ['Baking', 'cooking', 'eating'])
+// console.log(list)
+person.listPerson();
+
+
+
+
+
+/*
+
+var word = function (t) {
+    var text = t;
+
+    var translate = function (lang) {
+        if (lang ===  "pig latin") {
+            t = function() {
+                return text.substring(1) + text.substring(0,1) + "a";
+            };
+        }else {
+            t = function() {
+                return "I don't speak" + lang;
+            }
+        }
+        return t();
+    };
+
+    //objectပုံစံရေးတာ
+    return {
+        text : text,
+        translate : translate,
+    }
+};
+
+var ourText = word("hello"); // out out  [ elloha ]
+ourText.translate("pig latin");
+// console.log(ourText.translate("pig latin"));
+
+*/
+
+
+/*
+let languages = ["html", "css", "js"];
+//map(copy)
+languages = languages.map(function(lang) {
+    return 'I Love ' + lang;
+})
+console.log(languages);
+*/
+
